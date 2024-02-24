a8538796eb1b7dfd5845cbf4d072f6b8344361c0
Rochdy-max <rochdy.bachabi@epitech.eu>
2024-02-24 20:22:42 +0100

    feat: define cellpos_t to represent a cell inside a sandpile

--- include/sandpile.h
+++ include/sandpile.h
@@ -6,6 +6,12 @@ typedef struct sandpile_t
     int** mtx;
 }sandpile_t;
 
+typedef struct cellpos_t
+{
+    int row;
+    int col;
+}cellpos_t;
+
 sandpile_t create_sandpile(int height, int width, int cell_maxval, int** mtx);
 void destroy_sandpile(sandpile_t* pile);
 void normalize_sandpile(sandpile_t* pile);
